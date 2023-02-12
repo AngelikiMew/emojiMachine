8c34189ed6f7378e494353b2caafed22f3665375
AngelikiMew <angelikipapadaki@icloud.com>
2023-02-12 10:56:33 +0200

    new btns

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const emojiContainer=document.getElementById('emojiContainer');
 const pushBtn= document.getElementById('push-btn');
 const emojiInput = document.getElementById('emoji-input');
 const unshiftBtn= document.getElementById('unshift-btn');
-
+const popBtn= document.getElementById('pop-btn');
+const shiftBtn= document.getElementById('shift-btn')
 
 function renderArr(){
     emojiContainer.innerHTML='';
     for ( let i = 0 ; i < myEmojis.length; i++){
-    //console.log (myEmojis[i]);
-   // emojiContainer.innerHTML += `<span> ${myEmojis[i]} </span>`
+        //console.log (myEmojis[i]);
+        // emojiContainer.innerHTML += `<span> ${myEmojis[i]} </span>`
         let emoji = document.createElement("span");
         emoji.textContent= myEmojis[i];
         emojiContainer.appendChild(emoji);
@@ -19,12 +20,12 @@ function renderArr(){
 
 pushBtn.addEventListener('click', function (){
     if (emojiInput.value!='' && emojiInput.value!=' ' ){
-   // console.log(emojiInput.value);
-    myEmojis.push(emojiInput.value);
-    console.log(myEmojis);
-    emojiInput.value='';
-    renderArr();
-    }
+    // console.log(emojiInput.value);
+        myEmojis.push(emojiInput.value);
+        console.log(myEmojis);
+        emojiInput.value='';
+        renderArr();
+        }
 })
 
 unshiftBtn.addEventListener('click', function() {
@@ -37,3 +38,22 @@ unshiftBtn.addEventListener('click', function() {
          renderArr();
          }
      })
+
+popBtn.addEventListener('click', function() {
+
+         myEmojis.pop();
+         console.log(myEmojis);
+         emojiInput.value='';
+         renderArr();
+
+})
+
+shiftBtn.addEventListener('click', function() {
+
+         myEmojis.shift();
+         console.log(myEmojis);
+         emojiInput.value='';
+         renderArr();
+
+})
+    
