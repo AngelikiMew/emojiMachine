dc93b3b652362d94562618b2df561c5aa0302afc
AngelikiMew <angelikipapadaki@icloud.com>
2023-02-12 08:45:22 +0200

    first push

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const myEmojis = ["👨‍💻", "⛷", "🍲"];
+const emojiContainer=document.getElementById('emojiContainer');
 
+for ( let i = 0 ; i < myEmojis.length; i++){
+    //console.log (myEmojis[i]);
+   // emojiContainer.innerHTML += `<span> ${myEmojis[i]} </span>`
+
+    let emoji = document.createElement("span");
+    emoji.textContent= myEmojis[i]
+    emojiContainer.appendChild(emoji)
+
+
+}
